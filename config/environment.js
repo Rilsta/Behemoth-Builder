9cc95669d17e901ccea2f437b3b137d10e7c8e6e
rilsta <rstarnes9@gmail.com>
2016-04-11 10:10:21 -0700

    add firebase url

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'monster-creator',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://monstercreator.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
