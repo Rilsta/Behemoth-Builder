bd4417fe88277fb18e57021402aa44eea6ae8185
rilsta <rstarnes9@gmail.com>
2016-04-14 09:59:14 -0700

    add ability to select specific drop down items

--- app/components/monster-new.js
+++ app/components/monster-new.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  part: "Square",
-  parentCo: '',
   actions: {
-    choosePart(part){
-      this.set("part", part);
+    chooseShape(shape) {
+      debugger;
+      this.set.toString(shape);
     }
   }
 });
