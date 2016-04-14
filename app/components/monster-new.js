e752dcd6b575494647bb01bc21b2db32ade0f27e
rilsta <rstarnes9@gmail.com>
2016-04-13 21:29:46 -0700

    continued dropdown work

--- app/components/monster-new.js
+++ app/components/monster-new.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  part: "Square",
+  parentCo: '',
   actions: {
-        choosePart(){}
+    choosePart(part){
+      this.set("part", part);
+    }
   }
 });
