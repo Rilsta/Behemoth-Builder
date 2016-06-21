da9181ac7ca4dfd0a3ba1b8b0d9ac856d9edda54
rilsta <rstarnes9@gmail.com>
2016-06-21 13:55:56 -0700

    add a hide/show button for the traits form using liquid fire

--- app/components/monster-new.js
+++ app/components/monster-new.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isTraitShowing: false,
+  actions: {
+    traitShow: function() {
+      this.set('isTraitShowing', true);
+    }
+  }
 });
